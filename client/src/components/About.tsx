f8b2a92bb0d8890ebe606d55f55b78bd110b4fb4
Carlos Fabián Melgarejo Agudelo <cmelgarejobm@gmail.com>
2023-09-20 16:23:50 -0500

    About (authors)

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+
+const authors = [
+  {
+    nombre: "Carlos Fabian Melgarejo Agudelo",
+    linkedin: "@fabian",
+    linkedinUrl: "@fabian",
+    github: "@fabian",
+    githubUrl: "@fabian",
+  },
+];
 
 export default function About() {
-  return (
-    <div>Acerca de Nosotros</div>
-  )
+  return <div>Acerca de Nosotros</div>;
+  
 }
