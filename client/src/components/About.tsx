592078f96cdd1a2676565992f442c9130144cd75
Carlos Fabián Melgarejo Agudelo <cmelgarejobm@gmail.com>
2023-09-25 14:36:03 -0500

    Retired Card About Component

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -27,27 +27,22 @@ export default function About() {
   return (
     <Container>
       <Box
-        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        <Card sx={{ maxWidth: 345, marginTop: "20px" }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              src="../image/foto_linkedin.webp"
-              alt="image"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {authorFabian.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {authorFabian.linkedin}
-                {authorFabian.github}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
+        <Typography gutterBottom variant="h5" component="div">
+          {authorFabian.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {authorFabian.linkedin}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {authorFabian.github}
+        </Typography>
       </Box>
     </Container>
   );
