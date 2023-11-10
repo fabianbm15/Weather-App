1e1f613263d911267f168a115d01816aecacd9bf
Carlos Fabián Melgarejo Agudelo <cmelgarejobm@gmail.com>
2023-11-10 07:49:13 -0500

    Footer Details

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import "./Footer.css";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -13,126 +13,113 @@ import Button from "@mui/material/Button";
 // import Tooltip from '@mui/material/Tooltip';
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
-import makeStyles from "@mui/material/styles/makeStyles";
 
 const pages = ["Acerca de Nosotros"];
 
 export default function Footer() {
-    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
 
-    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-       setAnchorElNav(event.currentTarget);
-    };
- 
-    const handleCloseNavMenu = () => {
-       setAnchorElNav(null);
-    };
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
   return (
-    (
-        <AppBar position="static" className="appBarNavBar">
-           <Container className="containerNavBar">
-              <Toolbar disableGutters>
-                 {/* Ordenador */}
-                 
-                 <Typography
-                    component="a"
-                    href="/"
-                    className="titleNavBar"
-                    sx={{
-                       display: { xs: "none", md: "flex" },
-                    }}
-                 >
-                    Weather
-                 </Typography>
-                 <Typography
-                    component="a"
-                    href="/"
-                    className="titleNavBar"
-                    sx={{
-                       display: { xs: "none", md: "flex" },
-                       fontWeight: "500",
-                    }}
-                 >
-                    App
-                 </Typography>
-  
-                 <Box className="boxItemsNavBar" sx={{ display: { xs: "none", md: "flex" } }}>
-                    {pages.map((page) => (
-                       <Button
-                          key={page}
-                          onClick={handleCloseNavMenu}
-                          className="menuItem"
-                          sx={{ display: "block" }}
-                       >
-                          {page}
-                       </Button>
-                    ))}
-                 </Box>
-  
-                 {/* Móvil */}
-                 <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-                 <Typography
-                    variant="h5"
-                    noWrap
-                    component="a"
-                    href=""
-                    sx={{
-                       mr: 2,
-                       display: { xs: "flex", md: "none" },
-                       flexGrow: 1,
-                       fontFamily: "monospace",
-                       fontWeight: 700,
-                       letterSpacing: ".3rem",
-                       color: "inherit",
-                       textDecoration: "none",
-                    }}
-                 >
-                    LOGO
-                 </Typography>
-                 {/* Móvil */}
-                 <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-                    <IconButton
-                       size="large"
-                       aria-label="account of current user"
-                       aria-controls="menu-appbar"
-                       aria-haspopup="true"
-                       onClick={handleOpenNavMenu}
-                       color="inherit"
-                    >
-                       <MenuIcon />
-                    </IconButton>
-                    <Menu
-                       id="menu-appbar"
-                       anchorEl={anchorElNav}
-                       anchorOrigin={{
-                          vertical: "bottom",
-                          horizontal: "left",
-                       }}
-                       keepMounted
-                       transformOrigin={{
-                          vertical: "top",
-                          horizontal: "left",
-                       }}
-                       open={Boolean(anchorElNav)}
-                       onClose={handleCloseNavMenu}
-                       sx={{
-                          display: { xs: "block", md: "none" },
-                       }}
-                    >
-                       {pages.map((page) => (
-                          <MenuItem key={page} onClick={handleCloseNavMenu}>
-                             <Typography className="menuItem" textAlign="center">
-                                {page}
-                             </Typography>
-                          </MenuItem>
-                       ))}
-                    </Menu>
-                 </Box>
-                    
-              </Toolbar>
-           </Container>
-        </AppBar>
-     )
-  )
-}
+    <AppBar position="static" className="appBarFooter">
+      <Container className="containerFooter">
+        <Toolbar disableGutters>
+          {/* Ordenador */}
+          <Box className="boxFooter1">
+            <Typography
+              component="a"
+              href="/"
+              className="titleNavBar"
+              sx={{
+                display: { xs: "none", md: "flex" },
+              }}
+            >
+              Weather
+            </Typography>
+            <Typography
+              component="a"
+              href="/"
+              className="titleNavBar"
+              sx={{
+                display: { xs: "none", md: "flex" },
+                fontWeight: "500",
+              }}
+            >
+              App
+            </Typography>
+          </Box>
 
+          <Box className="boxFooter2">
+            {pages.map((page) => (
+              <Button key={page} onClick={handleCloseNavMenu} className="menuItem" sx={{ display: "block" }}>
+                {page}
+              </Button>
+            ))}
+            <Typography className="nameFooter" variant="h6" noWrap>
+              Fabian Melgarejo
+            </Typography>
+          </Box>
+
+          {/* Móvil */}
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+          {/* Móvil */}
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography className="menuItem" textAlign="center">
+                    {page}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
