5c08b08a5312c87b6dc0e912586d6d51dc9ef585
Carlos Fabián Melgarejo Agudelo <cmelgarejobm@gmail.com>
2023-11-12 10:15:57 -0500

    Details Component

--- client/src/components/Details.tsx
+++ client/src/components/Details.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
+import Container from "@mui/material/Container";
+import { Box } from "@mui/material";
+import { Typography } from "@mui/material";
+import "./Details.css"
 
 export default function Details() {
   return (
-    <div>Details</div>
-  )
+    <Container className="containerDetails">
+      <Box className="boxDetailsInfo">
+        <Typography className="cityNameDetails" variant="h2">Ocamonte, Santander</Typography>
+        <Box className="iconAndWeather">
+          <img src="" alt="no image" />
+          <Typography variant="h4">22 °C</Typography>
+          <Typography className="textInfoDetails" variant="h6">Lluvia Ligera</Typography>
+        </Box>
+        <Box>
+          <Typography className="textInfoDetails" variant="h6">Se siente como: </Typography>
+          <Typography className="textInfoDetails" variant="h6">Humedad: </Typography>
+          <Typography className="textInfoDetails" variant="h6">Presión </Typography>
+          <Typography className="textInfoDetails" variant="h6">Visibilidad: </Typography>
+          <Typography className="textInfoDetails" variant="h6">Velocidad del viento: </Typography>
+        </Box>
+      </Box>
+      <Box className="boxDetailsMap">
+        MAPA
+      </Box>
+    </Container>
+  );
 }
