ff52688e90604697e78d2ed82b6ecf73e060750f
Carlos Fabián Melgarejo Agudelo <cmelgarejobm@gmail.com>
2023-11-16 21:26:51 -0500

    City Interface

--- api/src/types.d.ts
+++ api/src/types.d.ts
@@ -8,5 +8,14 @@ export interface DiaryEntry {
   comment: string
 }
 
+export interface City {
+  "description": "nubes",
+  "temp": 17.79,
+  "feels_like": 18.05,
+  "pressure": 1015,
+  "humidity": 93,
+  "speed": 1.34,
+}
+
 export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, 'comment'>
 export type NewDiaryEntry = Omit<DiaryEntry, 'id'>
